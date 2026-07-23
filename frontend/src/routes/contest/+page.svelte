636b13fecddbe24d21833db29d6cd1c84ffe5857
pat <73502164+nyapat@users.noreply.github.com>
2026-07-23 20:24:59 +1000

    feat(contests page): update page

--- frontend/src/routes/contest/+page.svelte
+++ frontend/src/routes/contest/+page.svelte
@@ -16,52 +16,22 @@
 </script>
 
 <div class="container">
-    <h1 class="text-5xl font-bold text-[#5b8ddb] mb-10 text-center">
-        CPMSoc Contests
+    <h1 class="text-5xl font-bold text-[#5b8ddb] mb-5 text-center">
+        CPMSoc Contests of 2026
     </h1>
 
-    <div class="mb-8 w-full max-w-4xl text-center mt-10">
-        <div class="flex flex-row justify-center px-4 sm:px-10 gap-10">
-            <p class="text-3xl font-bold text-[#5b8ddb] mb-4">
-                <a
-                    class="underline"
-                    href="https://afc26.unswcpmsoc.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    April Fools Contest 2026
-                </a>
-            </p>
-        </div>
-    </div>
+    <h3 class="text-xl font-bold mb-5 text-center">
+        Want to find past contests? <a class="underline" href="https://unswcpmsoc.com/competitions">Head here</a>
+    </h3>
 
-    <div class="mb-2 w-full max-w-4xl text-center mt-10">
-        <h2 class="text-3xl font-bold text-[#5b8ddb]">
-            IMC Contest (Ended)
-        </h2>
-
-        <p class="text-2xl leading-relaxed py-6">
-            Choose your division
-        </p>
-
-        <div class="flex flex-row justify-center px-4 sm:px-10 gap-10">
-            <p class="text-3xl leading-relaxed">
-                <a
-                    class="underline"
-                    href="https://beginner.unswcpmsoc.com"
-                >
-                    beginner
-                </a>
-            </p>
-
-            <p class="text-3xl leading-relaxed">
-                <a
-                    class="underline"
-                    href="https://advanced.unswcpmsoc.com"
-                >
-                    advanced
-                </a>
-            </p>
-        </div>
+    <div>
+        <ul>
+            <li class="text-3xl font-bold text-[#5b8ddb] mb-4"><a class="underline" href="https://witcontest.unswcpmsoc.com/">WIT Contest (CPMSoc x WITSoc T2 W8)</a></li>
+            <li class="text-3xl font-bold text-[#5b8ddb] mb-4"><a class="underline" href="https://cookie.unswcpmsoc.com/">Cookie Code Crumble (CPMSoc x SecSoc T2 W8)</a></li>
+            <li class="text-3xl font-bold text-[#5b8ddb] mb-4"><a class="underline" href="https://duck.unswcpmsoc.com/">Duck Contest (Launch Week T2)</a></li>
+            <li class="text-3xl font-bold text-[#5b8ddb] mb-4"><a class="underline" href="https://afc26.unswcpmsoc.com/">April Fools Contest 2026 (T1)</a></li>
+            <li class="text-3xl font-bold text-[#5b8ddb] mb-4">IMC Contest: <a class="underline" href="https://beginner.unswcpmsoc.com/">Beginner</a>, <a class="underline" href="https://advanced.unswcpmsoc.com/">Advanced</a></li>
+            <li class="text-3xl font-bold text-[#5b8ddb] mb-4"><a class="underline" href="https://horse.unswcpmsoc.com/">Horse Contest (O-Week T1)</a></li>
+        </ul>
     </div>
 </div>
